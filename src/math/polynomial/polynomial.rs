1530dee05d6b8bcf83b558f31ee2189b3499bdb4
lead <139946934+teamlab1@users.noreply.github.com>
2024-07-03 10:31:27 +0800

    struct name fix

--- src/math/polynomial/polynomial.rs
+++ src/math/polynomial/polynomial.rs
@@ -1,16 +1,20 @@
+use std::ops;
+
 use std::fmt::{self, Display};
 use std::str::FromStr;
 extern crate serde_json;
 
-pub struct Polinomial<const ORDER: usize>(Box<[u64; ORDER]>);
+pub struct Polynomial<const ORDER: usize>(Box<[u64; ORDER]>);
 
-impl<const ORDER: usize> Polinomial<ORDER> {
+impl<const ORDER: usize> Polynomial<ORDER> {
     fn new(data: Box<[u64; ORDER]>) -> Self {
-        Polinomial(data)
+        Polynomial(data)
     }
 }
 
-impl<const Polynomialsize: usize> Display for Polinomial<Polynomialsize> {
+// Serialization
+
+impl<const Polynomialsize: usize> Display for Polynomial<Polynomialsize> {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         write!(
             formatter,
@@ -22,12 +26,12 @@ impl<const Polynomialsize: usize> Display for Polinomial<Polynomialsize> {
     }
 }
 
-impl<const Polynomialsize: usize> FromStr for Polinomial<Polynomialsize> {
+impl<const Polynomialsize: usize> FromStr for Polynomial<Polynomialsize> {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let data: Vec<u64> = serde_json::from_str(s).unwrap();
-        Ok(Polinomial::new(Box::new(data.try_into().unwrap())))
+        Ok(Polynomial::new(Box::new(data.try_into().unwrap())))
     }
 }
 
@@ -35,9 +39,11 @@ impl<const Polynomialsize: usize> FromStr for Polinomial<Polynomialsize> {
 fn test_polynomial_to_str_serialization() {
     // todo make iterative, make random
     let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    let ct: Polinomial<10> = Polinomial::new(Box::new(a));
+    let ct: Polynomial<10> = Polynomial::new(Box::new(a));
 
     let serialized = ct.to_string();
-    let deserialized: Polinomial<10> = FromStr::from_str(&serialized).unwrap();
+    let deserialized: Polynomial<10> = FromStr::from_str(&serialized).unwrap();
     assert_eq!(ct.0, deserialized.0);
 }
+
+
